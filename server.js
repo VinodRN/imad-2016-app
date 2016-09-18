29c0b053eeddf4ff514a5495ea44c761b960f70b
VinodRN <vinod.infotech@gmail.com>
2016-09-18 23:12:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var articleone = {
     title: 'Article one ! VinodRN' , 
     heading:'Article one',
     date : 'Sep 5 2016',
-    Content:`
+    content:`
         <p>
         This is the content for my first article! This is the content for my first article! This is the content for my first article! This is the content for my first article! 
         </p>
