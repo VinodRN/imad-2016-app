5ea161d02ea9f0889ab36b28ae14bfd70fd020cd
VinodRN <vinod.infotech@gmail.com>
2016-10-02 15:14:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
 console.log('Loaded!');
+
+// change the text
 var element = document.getElementById('text-area'
     );
 element.innerHTML = 'newValue';
+
+//Move the image
+var img = document.getElementById('img-area');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+}
