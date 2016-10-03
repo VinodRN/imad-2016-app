90e1582228bbb06f30e853c7e2696966b55c6a97
VinodRN <vinod.infotech@gmail.com>
2016-10-04 00:45:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,7 @@ button.onclick = function(){
           //take some action
           if(request.status === 200 ){
               counter = request.responseText;
+              console.log(counter);
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
           }
