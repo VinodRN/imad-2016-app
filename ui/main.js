3d417fe2d54abfbaf194cb88b6caeb3709c60cc0
VinodRN <vinod.infotech@gmail.com>
2016-10-04 00:54:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,7 @@ button.onclick = function(){
     {
       if(request.readystate === XMLHttpRequest.DONE)
       {
+          console.log(clicked);
           //take some action
           if(request.status === 200 )
           {
