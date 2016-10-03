d08258c95c5c0fff77735c63f52405f33c0e31c6
VinodRN <vinod.infotech@gmail.com>
2016-10-04 00:28:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,19 @@ var counter = 0 ;
 
 button.onclick = function(){
 // make a request to the counter endpoint
-// capture the response and store it in a variable.
-//render the variable in the correct span
-counter= couter + 1; 
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
 
+// capture the response and store it in a variable.
+    request.onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+          //take some action
+          if(request.status === 200 ){
+              counter= couter + 1; 
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }  
+    };
+    request.open('GET', 'http://vinodrn.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
