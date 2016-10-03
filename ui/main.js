cd4679c0e2dc1427887953a1ebb0fdcb600f8a0f
VinodRN <vinod.infotech@gmail.com>
2016-10-04 00:20:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,13 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0 ;
 
-// change the text
-var element = document.getElementById('text-area'
-    );
-element.innerHTML = 'newValue';
+button.onclick = function(){
+// make a request to the counter endpoint
+// capture the response and store it in a variable.
+//render the variable in the correct span
+counter= couter + 1; 
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
 
-//Move the image
-var img = document.getElementById('img-area');
-var marginLeft= 0 ;
-
-function moveRight()
-{
-    marginLeft = marginLeft + 5 ; 
-    img.style.marginLeft = marginLeft +'px' ; 
-    
-}
-img.onclick = function() {
-    var internal = setInterval(moveRight,50)
 }
