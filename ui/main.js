8fcd1e2c9f671b70840bf2442b0e87ce55d190ae
VinodRN <vinod.infotech@gmail.com>
2016-10-04 17:15:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementByID('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
