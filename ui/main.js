8a59ddb407fb580edffc7b3bab5289701bf61bdb
VinodRN <vinod.infotech@gmail.com>
2016-10-04 21:05:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,15 +28,29 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
-    //make a request to the server and send the name
-    
-    //Capture the list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for ( var i =0 ; i<names.length ; i++)
+    // make a request to the counter endpoint
+    var request = new XMLHttpRequest();
+
+// capture the response and store it in a variable.
+    request.onreadystatechange = function()
     {
-        list += '<li>' +names[i] +'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          //take some action
+          if(request.status === 200){
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for ( var i =0 ; i<names.length ; i++)
+            {
+                list += '<li>' +names[i] +'</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+          }
+      }  
+    };
+    
+    request.open('GET', 'http://vinodrn.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
